ec7de63daa66f65d6cd0ea4cf48ae840d625eaa0
KubaTaba1uga <jakub@buczynski.net.pl>
2021-12-18 23:11:12 +0100

    add iterators

---
+++ iterators/how_iteration_works.py
@@ -0,0 +1,60 @@
+class IterationExample:
+    def __init__(self):
+        self.start = 0
+        self.stop = 5
+
+    def __iter__(self):
+        print("__iter__ revoked")
+        return self.__class__()
+
+    def __next__(self):
+        if self.start == self.stop:
+            raise StopIteration
+        print("__next__ revoked {} time".format(self.start))
+        self.start += 1
+
+
+iterator = IterationExample()
+
+
+"""
+	Manual example of for loop protocol
+"""
+print("Manual example of for loop protocol")
+
+# First iterator is being created
+real_iterator = iter(iterator)
+
+for i in range(iterator.stop):
+    # On each iteration next()
+    #	is revoked
+    next(iterator)
+
+# For loop stops execution when
+#	StopIteration is raised
+#	as iterator is exausted
+#	each next() revoke will
+#	raise error also
+try:
+    next(iterator)
+except StopIteration:
+    print("Iterator is exausted")
+
+"""
+	The same behaviour shown
+		using for loop protocol
+"""
+print("\nReal example of for loop protocol")
+
+for element in iterator:
+    pass
+else:
+    # Else is revoked after
+    #	for loop end as for
+    #	loop protocol is
+    #	using if in each
+    #	iteration
+    try:
+        next(iterator)
+    except StopIteration:
+        print("Iterator is exausted")
---
+++ iterators/my_own_iterable__iter__.py
@@ -0,0 +1,51 @@
+"""
+    Iteration protocol, checks object for:
+        1. __next__ or __iter__
+        2. __len__ and __getitem__
+
+    According to 1. there are two ways of
+        creating iterators in python.
+       
+"""
+
+# Let's imagine a object which should generate
+#  dates from desired range, example:
+#    since 10-10-2021
+#    till  15-12-2021
+
+from datetime import date, timedelta
+
+
+class DatesIterator:
+    """ Create iterable object
+         using __iter__ magic method
+         has one advance, as new 
+         iterator is created on each 
+         iter() revoke
+    """
+
+    def __init__(self, start_at: date,
+                 stop_at: date):
+        self.start_at = start_at
+        self.stop_at = stop_at
+
+    def __iter__(self):
+        print("iterator revoked")
+        present_day = self.start_at
+        while present_day < self.stop_at:
+            yield present_day
+            present_day += timedelta(days=1)
+
+
+my_days = DatesIterator(start_at=date(1992, 12, 12), stop_at=date(1993, 1, 1))
+
+for day in my_days:
+    # Generator will be created again
+    #  on any iter() revoke
+    for day in my_days:
+        # Once this loop revoke iter()
+        #  new generator will be created
+        # In consequence next() won't be
+        #  revoked on the object but on
+        #  generator
+        print(day)
